e52c3a2e748d5c09fd9371bd927cc0cddf964180
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2018-11-18 00:30:35 +0200

    Initial commit, v1.0.0
    - Moved into its own npm module from openapi-backend utils
    - Added tests

---
+++ src/mock.test.ts
@@ -0,0 +1,397 @@
+import { mock, SchemaLike } from './index';
+
+describe('mock', () => {
+  describe('null', () => {
+    test('null primitive', () => {
+      const schema: SchemaLike = {
+        type: 'null',
+      };
+      expect(mock(schema)).toStrictEqual(null);
+    });
+  });
+
+  describe('boolean', () => {
+    test('boolean primitive', () => {
+      const schema: SchemaLike = {
+        type: 'boolean',
+      };
+      expect(mock(schema)).toStrictEqual(true);
+    });
+
+    test('boolean with default', () => {
+      const schema: SchemaLike = {
+        type: 'boolean',
+        default: false,
+      };
+      expect(mock(schema)).toStrictEqual(false);
+    });
+  });
+
+  describe('number', () => {
+    test('number primitive', () => {
+      const schema: SchemaLike = {
+        type: 'number',
+      };
+      expect(mock(schema)).toStrictEqual(0);
+    });
+
+    test('number with minimum', () => {
+      const schema: SchemaLike = {
+        type: 'number',
+        minimum: 3.14,
+      };
+      expect(mock(schema)).toStrictEqual(3.14);
+    });
+
+    test('number with maximum', () => {
+      const schema: SchemaLike = {
+        type: 'number',
+        maximum: -14.4,
+      };
+      expect(mock(schema)).toStrictEqual(-14.4);
+    });
+
+    test('number with multipleOf', () => {
+      const schema: SchemaLike = {
+        type: 'number',
+        multipleOf: 0.6,
+        minimum: 1,
+      };
+      expect(mock(schema)).toStrictEqual(1.2);
+    });
+  });
+
+  describe('integer', () => {
+    test('integer primitive', () => {
+      const schema: SchemaLike = {
+        type: 'integer',
+      };
+      expect(mock(schema)).toStrictEqual(0);
+    });
+
+    test('integer with maximum', () => {
+      const schema: SchemaLike = {
+        type: 'integer',
+        maximum: -1,
+      };
+      expect(mock(schema)).toStrictEqual(-1);
+    });
+
+    test('integer with maximum exclusiveMaximum=true', () => {
+      const schema: SchemaLike = {
+        type: 'integer',
+        maximum: -1,
+        exclusiveMaximum: true,
+      };
+      expect(mock(schema)).toStrictEqual(-2);
+    });
+
+    test('integer with minimum', () => {
+      const schema: SchemaLike = {
+        type: 'integer',
+        minimum: 1,
+      };
+      expect(mock(schema)).toStrictEqual(1);
+    });
+
+    test('integer with minimum exclusiveMinimum=true', () => {
+      const schema: SchemaLike = {
+        type: 'integer',
+        minimum: 1,
+        exclusiveMinimum: true,
+      };
+      expect(mock(schema)).toStrictEqual(2);
+    });
+
+    test('integer with multipleOf', () => {
+      const schema: SchemaLike = {
+        type: 'integer',
+        multipleOf: 5,
+        minimum: 5,
+        exclusiveMinimum: true,
+      };
+      expect(mock(schema)).toStrictEqual(10);
+    });
+  });
+
+  describe('string', () => {
+    test('string primitive', () => {
+      const schema: SchemaLike = {
+        type: 'string',
+      };
+      expect(mock(schema)).toStrictEqual('string');
+    });
+
+    test('string with maxLength', () => {
+      const schema: SchemaLike = {
+        type: 'string',
+        maxLength: 3,
+      };
+      expect(mock(schema)).toStrictEqual('str');
+    });
+
+    test('string with minLength', () => {
+      const schema: SchemaLike = {
+        type: 'string',
+        minLength: 9,
+      };
+      expect(mock(schema)).toStrictEqual('stringstr');
+    });
+
+    test('string with format', () => {
+      const schema: SchemaLike = {
+        type: 'string',
+        format: 'date-time',
+      };
+      expect(mock(schema)).toStrictEqual(new Date('1970-01-01').toJSON());
+    });
+  });
+
+  describe('object', () => {
+    test('object primitive', () => {
+      const schema: SchemaLike = {
+        type: 'object',
+      };
+      expect(mock(schema)).toStrictEqual({});
+    });
+
+    test('object with properties', () => {
+      const schema: SchemaLike = {
+        type: 'object',
+        properties: {
+          numberProp: {
+            type: 'number',
+          },
+          stringProp: {
+            type: 'string',
+          },
+        },
+      };
+      expect(mock(schema)).toStrictEqual({ numberProp: 0, stringProp: 'string' });
+    });
+
+    test('object with nested properties', () => {
+      const schema: SchemaLike = {
+        type: 'object',
+        properties: {
+          child: {
+            type: 'object',
+            properties: {
+              numberProp: {
+                type: 'number',
+              },
+              stringProp: {
+                type: 'string',
+              },
+            },
+          },
+        },
+      };
+      expect(mock(schema)).toStrictEqual({ child: { numberProp: 0, stringProp: 'string' } });
+    });
+  });
+
+  describe('array', () => {
+    test('array with items', () => {
+      const schema: SchemaLike = {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      };
+      expect(mock(schema)).toStrictEqual(['string']);
+    });
+
+    test('array with minItems', () => {
+      const schema: SchemaLike = {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+        minItems: 3,
+      };
+      expect(mock(schema)).toStrictEqual(['string', 'string', 'string']);
+    });
+
+    test('array with oneOf', () => {
+      const schema: SchemaLike = {
+        type: 'array',
+        items: {
+          type: 'string',
+          oneOf: [
+            {
+              type: 'string',
+              example: 'string1',
+            },
+            {
+              type: 'string',
+              example: 'string2',
+            },
+          ],
+        },
+      };
+      expect(mock(schema)).toStrictEqual(['string1']);
+    });
+
+    test('array with allOf', () => {
+      const schema: SchemaLike = {
+        type: 'array',
+        items: {
+          type: 'string',
+          allOf: [
+            {
+              type: 'string',
+              example: 'string1',
+            },
+            {
+              type: 'string',
+              example: 'string2',
+            },
+          ],
+        },
+      };
+      expect(mock(schema)).toStrictEqual(['string1', 'string2']);
+    });
+
+    test('array with anyOf', () => {
+      const schema: SchemaLike = {
+        type: 'array',
+        items: {
+          type: 'string',
+          anyOf: [
+            {
+              type: 'string',
+              example: 'string1',
+            },
+            {
+              type: 'string',
+              example: 'string2',
+            },
+          ],
+        },
+      };
+      expect(mock(schema)).toStrictEqual(['string1', 'string2']);
+    });
+
+    test('array with anyOf + maxItems', () => {
+      const schema: SchemaLike = {
+        type: 'array',
+        items: {
+          type: 'string',
+          anyOf: [
+            {
+              type: 'string',
+              example: 'string1',
+            },
+            {
+              type: 'string',
+              example: 'string2',
+            },
+          ],
+        },
+        maxItems: 1,
+      };
+      expect(mock(schema)).toStrictEqual(['string1']);
+    });
+
+    test('array with anyOf + minItems', () => {
+      const schema: SchemaLike = {
+        type: 'array',
+        items: {
+          type: 'string',
+          anyOf: [
+            {
+              type: 'string',
+              example: 'string1',
+            },
+            {
+              type: 'string',
+              example: 'string2',
+            },
+          ],
+        },
+        minItems: 4,
+      };
+      expect(mock(schema)).toStrictEqual(['string1', 'string2', 'string2', 'string2']);
+    });
+
+    test('array with oneOf + minItems', () => {
+      const schema: SchemaLike = {
+        type: 'array',
+        items: {
+          type: 'string',
+          oneOf: [
+            {
+              type: 'string',
+              example: 'string1',
+            },
+            {
+              type: 'string',
+              example: 'string2',
+            },
+          ],
+        },
+        minItems: 4,
+      };
+      expect(mock(schema)).toStrictEqual(['string1', 'string1', 'string1', 'string1']);
+    });
+  });
+
+  describe('anyOf', () => {
+    test('anyOf with options', () => {
+      const schema: SchemaLike = {
+        type: 'object',
+        anyOf: [
+          {
+            type: 'object',
+            properties: {
+              propA: {
+                type: 'string',
+                example: 'a',
+              },
+            },
+          },
+          {
+            type: 'object',
+            properties: {
+              propB: {
+                type: 'string',
+                example: 'b',
+              },
+            },
+          },
+        ],
+      };
+      expect(mock(schema)).toStrictEqual({ propA: 'a' });
+    });
+  });
+
+  describe('oneOf', () => {
+    test('oneOf with options', () => {
+      const schema: SchemaLike = {
+        type: 'object',
+        oneOf: [
+          {
+            type: 'object',
+            properties: {
+              propA: {
+                type: 'string',
+                example: 'a',
+              },
+            },
+          },
+          {
+            type: 'object',
+            properties: {
+              propB: {
+                type: 'string',
+                example: 'b',
+              },
+            },
+          },
+        ],
+      };
+      expect(mock(schema)).toStrictEqual({ propA: 'a' });
+    });
+  });
+});
---
+++ src/mock.ts
@@ -0,0 +1,122 @@
+import _ from 'lodash';
+import { OpenAPIV3 } from 'openapi-types';
+
+export type SchemaLike = OpenAPIV3.SchemaObject;
+
+export function mock(schema: SchemaLike): any {
+  // use specified example
+  if (schema.example !== undefined) {
+    return schema.example;
+  }
+
+  // use default
+  if (schema.default !== undefined) {
+    return schema.default;
+  }
+
+  // oneOf, use first
+  if (schema.oneOf && schema.oneOf[0]) {
+    return mock(schema.oneOf[0] as SchemaLike);
+  }
+
+  // anyOf, use first
+  if (schema.anyOf && schema.anyOf[0]) {
+    return mock(schema.anyOf[0] as SchemaLike);
+  }
+
+  // get type, use first if array
+  const type = _.isArray(schema) ? _.first(schema.type) : schema.type;
+
+  if (type === 'object') {
+    const obj = schema as OpenAPIV3.NonArraySchemaObject;
+    const { properties } = obj;
+    if (!properties) {
+      return {};
+    }
+    return _.mapValues(properties, mock);
+  }
+
+  if (type === 'array') {
+    const array = schema as OpenAPIV3.ArraySchemaObject;
+    const items = array.items as SchemaLike;
+    if (!items) {
+      return [];
+    }
+    const examples = [];
+    let example = ((items.oneOf && items.oneOf[0]) || items) as SchemaLike;
+    if (items.anyOf || items.allOf) {
+      // include one of each example for anyOf and allOf
+      for (const option of items.anyOf || items.allOf) {
+        example = option as SchemaLike;
+        examples.push(mock(example));
+      }
+    }
+    // if minItems is set make sure we have at least that many items
+    const minItems = array.minItems || 1;
+    while (examples.length < minItems) {
+      examples.push(mock(example));
+    }
+    // limit to max items if applicable
+    return array.maxItems ? _.take(examples, array.maxItems) : examples;
+  }
+
+  if (_.isArray(schema.enum)) {
+    return schema.enum[0];
+  }
+
+  if (type === 'string') {
+    const { format } = schema;
+    const formatExamples: { [format: string]: string } = {
+      email: 'user@example.com',
+      hostname: 'example.com',
+      ipv4: '8.8.8.8',
+      ipv6: '2001:4860:4860::8888',
+      uri: 'https://example.com/path',
+      'uri-reference': '/path#anchor',
+      'uri-template': '/path/{param}',
+      'json-pointer': '/foo/bar',
+      'date-time': new Date('1970-01-01').toJSON(),
+      _default: 'string',
+    };
+    const val = formatExamples[format] || formatExamples._default;
+    const minln = !_.isNil(schema.minLength) ? schema.minLength : 0;
+    const maxln = !_.isNil(schema.maxLength) ? schema.maxLength : val.length;
+    if (val === formatExamples._default && val.length < minln) {
+      return _.padEnd(val, minln, val);
+    }
+    return val.substr(0, _.clamp(val.length, minln, maxln));
+  }
+
+  if (type === 'number') {
+    let min = schema.minimum ? schema.minimum : -Number.MAX_VALUE;
+    let max = schema.maximum ? schema.maximum : Number.MAX_VALUE;
+    if (schema.multipleOf) {
+      min = Math.ceil(min / schema.multipleOf) * schema.multipleOf;
+      max = Math.floor(max / schema.multipleOf) * schema.multipleOf;
+    }
+    return _.clamp(0, min, max);
+  }
+
+  if (type === 'integer') {
+    const schemaMin = schema.minimum && schema.exclusiveMinimum ? schema.minimum + 1 : schema.minimum;
+    const schemaMax = schema.maximum && schema.exclusiveMaximum ? schema.maximum - 1 : schema.maximum;
+    let min = !_.isNil(schemaMin) ? schemaMin : Number.MIN_SAFE_INTEGER;
+    let max = !_.isNil(schemaMax) ? schemaMax : Number.MAX_SAFE_INTEGER;
+    if (schema.multipleOf) {
+      min = Math.ceil(min / schema.multipleOf) * schema.multipleOf;
+      max = Math.floor(max / schema.multipleOf) * schema.multipleOf;
+    }
+    return _.clamp(0, min, max);
+  }
+
+  if (type === 'null') {
+    return null;
+  }
+
+  if (type === 'boolean') {
+    return true;
+  }
+
+  // unknown type
+  return {};
+}
